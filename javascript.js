383f40ab4f71d5fd9522e58c1ac9821c52bff089
xCubeArrow <1984flaker@gmail.com>
2020-01-19 21:33:05 +0100

    Completely redid the website so it isn't such a mess anymore

--- javascript.js
+++ javascript.js
@@ -1,11 +1,22 @@
 window.onscroll = function () {
-    const aboutcontainer = document.getElementById("aboutcontainer");
-    const toolsSection = document.getElementById("toolsSection");
-    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-        aboutcontainer.style.width = "100%";
-        toolsSection.style.opacity = "1";
-    } else {
-        toolsSection.style.opacity = "0";
-        aboutcontainer.style.width = "41.666667%";
+    const popout = document.getElementById("popout");
+    if(window.innerWidth > 800) {
+        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+            popout.style.width = "60%";
+            changeOpacity("1")
+
+        } else {
+            popout.style.width = "0";
+            changeOpacity("0")
+        }
     }
 };
+
+function changeOpacity(x) {
+    const children = popout.childNodes;
+    for (var i = 0; i < children.length; i++) {
+        if (children[i].nodeName.toLowerCase() === 'div') {
+            children[i].style.opacity = x;
+        }
+    }
+}
